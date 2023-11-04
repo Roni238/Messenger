32faeb9e929a4466a1e7d9c4f36a47ee34c64b4b
Anton Kostiuk <ak4932328@gmail.com>
2023-11-04 06:12:34 +0300

    112

--- vue.config.js
+++ vue.config.js
@@ -1,5 +1,5 @@
 const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
   transpileDependencies: true,
-  publicPath: `/messenger-vue3/`
+  publicPath: `/Messenger/`
 })
